af7b11a6e1de648024f35ce271dadbce75d2800a
vFISHv <vfishv@gmail.com>
2022-09-15 22:53:58 +0800

    androidx

--- app/src/main/java/win/lioil/autoInstall/InstallUtil.java
+++ app/src/main/java/win/lioil/autoInstall/InstallUtil.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.widget.Toast;
 
 import java.io.File;
